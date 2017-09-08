8a591d26986b04c3a345bafc340d35dbf702b696
Domenico Maisano <dev.dmaisano@gmail.com>
2017-09-07 23:12:48 -0400

    added menu.cpp

--- group/two-dim-vector/menu.cpp
+++ group/two-dim-vector/menu.cpp
@@ -4,18 +4,27 @@ using namespace std;
 
 int main() {
   vector<int> vect;
-  int userInput = -99;
+  int menuChoice = -99;
 
-  vect.push_back(1);
-
-  cout << "Size: " << vect.size() << "\n\n";
+  if(vect.size() > 0) {
+    cout << "Size: " << vect.size() << "\n\n";
+  }
   
-  while(userInput != -1) {
-    int menuIndex = 0;
+  while(menuChoice != -1) {
 
     if(vect.size() > 0) {
-
+      cout << "Enter 0 to display vectors.\n";
     }
+
+    cout << "Enter 1 to create a new vector.\n";
+    cout << "Enter 2 to delete a vector.\n";
+    cout << "Enter 3 to add two vectors.\n";
+    cout << "Enter 4 to subtract two vectors.\n";
+    cout << "Enter 5 to multiply a vector by a scalar.\n";
+    cout << "Enter 6 to multiply two vectors.\n";
+    cout << "Enter 7 to get the magnitude of a vector.\n";
+
+    cin >> menuChoice;
   }
   
   return 0;
