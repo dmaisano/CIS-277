3f2d14f31a30b2530c6aa4a907e4408b78f3e4ca
Domenico Maisano <dev.dmaisano@gmail.com>
2017-09-19 13:45:41 -0400

    removed clutter

--- misc/file-headers/print-array.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-void printArray(double* arr, int size) {
-  for(int i = 0; i < size; i++) {
-    cout << "Element: " << i << "\tValue: " << arr[i] << "\n";
-  }
-}
