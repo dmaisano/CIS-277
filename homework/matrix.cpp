80d01c6854fc1b0eca482912db879fa50b196fa8
Domenico Maisano <dev.dmaisano@gmail.com>
2017-09-21 23:26:37 -0400

    added matrix.cpp

---
+++ homework/matrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// template for the abstract data type 'MATRIX'
+struct MATRIX {
+  string name;
+  double a, b, c, d; 
+  // 2x2 matrix
+  // [a b]
+  // [c d]
+};
+
+int main() {
+  
+  
+  return 0;
+}
