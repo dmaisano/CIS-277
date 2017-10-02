4b3b2a4ae3c5328da6290cf38b185b11d5340cc2
Domenico Maisano <dev.dmaisano@gmail.com>
2017-10-02 15:26:47 -0400

    updated matrix.py

--- homework/matrix.py
+++ homework/matrix.py
@@ -1,13 +1,28 @@
 class Matrix:
-  def __init__(self, name, row, col):
-    self.name = name
-    self.row = row
-    self.col = col
-    self.matrix = [ list(range(row)), list(range(col)) ] # initializes matrix with default values
+  def __init__(self):
+    self.name = self.get_name()
+    self.size = self.get_size()
+    self.row = self.size[0]
+    self.col = self.size[1]
+    self.matrix = [ list(range(self.row)), list(range(self.col)) ] # initializes matrix with default values
     self.get_values()
-    print()
+    print() # prints a new line at the end of object construction
 
-  
+  # Returns the name for the matrix as per user input
+  def get_name(self):
+    name = input("Enter the name for the matrix: ")
+
+    return name
+
+  # Retuns a list of the rows and cols for the matrix
+  def get_size(self):
+    size = []
+    size.append(int(input("Enter the number of rows for matrix {0}: ".format(self.name))))
+    size.append(int(input("Enter the number of columns for matrix {0}: ".format(self.name))))
+
+    return size
+
+  # Gets values for the matrix as per user input
   def get_values(self):
     for x in range(self.row):
       for y in range(self.col):
@@ -15,6 +30,7 @@ class Matrix:
         num = int(input(string))
         self.matrix[x][y] = num
 
+  # Prints the values of the matrix
   def printMatrix(self):
     print("Matrix " + self.name + ":")
 
@@ -26,8 +42,12 @@ class Matrix:
         else:
           print("{0}]".format(self.matrix[x][y]), end="\n")
 
-toast = Matrix("toast", 2, 2)
+matrices = []
 
-toast.printMatrix()
+toast = Matrix()
+matrices.append(toast)
+
+for matrix in matrices:
+  matrix.printMatrix()
 
 print("\n")
