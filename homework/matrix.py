aed2ded9ad39e3d885b93f234808135da2a820f8
Domenico Maisano <dev.dmaisano@gmail.com>
2017-10-04 16:57:38 -0400

    updated two files

--- homework/matrix.py
+++ homework/matrix.py
@@ -1,11 +1,11 @@
 class Matrix:
-  def __init__(self, row=None, col=None, name=None):
+  def __init__(self, row=None, col=None, name=None, data=None):
     self.name = name
     self.row = row
     self.col = col
     self.get_name()
     self.get_size()
-    self.matrix = [] # initializes matrix with default values
+    self.matrix = [] or data # initializes matrix with default values
     #self.get_values()
     #print() # prints a new line at the end of object construction
 
@@ -35,8 +35,7 @@ class Matrix:
     for x in range(self.row):
       for y in range(self.col):
         string = "Enter a value for row {0} col {1}: ".format(x, y)
-        num = int(input(string))
-        self.matrix[x][y] = num
+        self.matrix[x][y] = int(input(string))
 
     print()
 
@@ -44,8 +43,8 @@ class Matrix:
   def printMatrix(self):
     print("Matrix {0}:".format(self.name))
 
-    for x in range(self.row):
-      for y in range(self.col):
+    for x in self.matrix:
+      for y in x:
         if(y == 0):
           print("[{0}".format(self.matrix[x][y]), end=" ")
         
@@ -54,32 +53,42 @@ class Matrix:
 
   def __add__(self, other):
     if (self.row == other.row and self.col == other.col):
-      m = Matrix(self.name, self.row, self.col)
+      values = [ list(range(self.row)), list(range(self.col)) ]
 
-      m.matrix = [ list(range(self.row)), list(range(self.col)) ]
+      for x in self.matrix:
+        for y in x:
+          values[x][y] = self.matrix[x][y] + other.matrix[x][y]
 
-      for x in range(self.row):
-        for y in range(self.col):
-          m.matrix[x][y] = self.matrix[x][y] + other.matrix[x][y]
-
-      return m
+      return Matrix(self.row, self.col, None, values)
 
     else:
       print("Cannot add the two matrices. Please make sure they are the same size!")
 
+  def __sub__(self, other):
+    if (self.row == other.row and self.col == other.col):
+      values = [ list(range(self.row)), list(range(self.col)) ]
+
+      for x in range(self.row):
+        for y in range(self.col):
+          values[x][y] = self.matrix[x][y] - other.matrix[x][y]
+
+      return Matrix(self.row, self.col, None, values)
+
+    else:
+      print("Cannot subtract the two matrices. Please make sure they are the same size!")
+
 matrices = []
 
-toast = Matrix(2, 2)
+toast = Matrix(3, 3)
 toast.get_values()
-eggs = Matrix(2, 2)
+eggs = Matrix(3, 3)
 eggs.get_values()
 
-m = Matrix(2, 2)
-
-m = toast + eggs
+sumMatrices = toast + eggs
 
 matrices.append(toast)
 matrices.append(eggs)
+matrices.append(sumMatrices)
 
 for matrix in matrices:
   matrix.printMatrix()
--- misc/test.py
+++ misc/test.py
@@ -1,3 +1,4 @@
+'''
 def squareNum(num=None):
   
   if (num == None):
@@ -11,3 +12,36 @@ def squareNum(num=None):
     print("Square of number is '{0}'".format(temp))
 
 squareNum(10)
+'''
+
+def createMatrix(row=None, col=None):
+  if(row == None and col == None):
+    row = input("Enter the rows for the list: ")
+    col = input("Enter the columns for the list: ")
+
+  matrix = [ list(range(row)), list(range(col, col*2)) ]
+
+  return matrix
+
+'''
+def getValues(matrix):
+  for x in range(matrix[0]):
+    for y in range(matrix[1]):
+      string = "Enter a value for row {0} col {1}: ".format(x, y)
+      matrix[x][y] = int(input(string))
+'''
+matrix = createMatrix(3, 3)
+
+for x in matrix:
+  for y in x:
+    print(x)
+
+print()
+
+for x in matrix:
+  for y in x:
+    print(y)
+
+#getValues(matrix)
+
+
