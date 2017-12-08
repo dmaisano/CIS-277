20e869bc327d3333f79e35328156098ff8f3b105
Domenico Maisano <dev.dmaisano@gmail.com>
2017-12-07 21:55:54 -0500

    added substring app

---
+++ homework/substring/app.js
@@ -0,0 +1,4 @@
+let fms = require('./substr.js');
+
+x = '1101aaa';
+console.log(fms(x));
---
+++ homework/substring/substr.js
@@ -0,0 +1,18 @@
+// finite state machine func() to find a substring containing '1101'
+function fms(str) {
+  str = str.split('') // splits the string up into a character array
+
+  .filter(char => char == '0' || char == '1') // filters the results of the array to include only binary numbers
+
+  .join(''); // joins the character array into one string contained inside of an array
+
+  return str.includes('1101') ? true : false; // returns true is the string contains a substring of '1101'
+
+  // The return statement above has the same behavior as the snippet below...
+  // if(str.str.includes('1101'))
+  //   return true
+
+  // return false
+}
+
+module.exports = fms;
