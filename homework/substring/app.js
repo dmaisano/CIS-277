4cb5841ba5163130326f097ff50ac20642a02ef0
Domenico Maisano <dev.dmaisano@gmail.com>
2017-12-11 19:16:18 -0500

    finished substring app

--- homework/substring/app.js
+++ homework/substring/app.js
@@ -1,4 +1,19 @@
-let fms = require('./substr.js');
+document.getElementById('output').style.visibility = 'hidden';
 
-x = '1101aaa';
-console.log(fms(x));
+function fms(str) {
+  str = str.split('').filter(char => char == '0' || char == '1').join('');
+
+  return str.includes('1101') ? true : false;
+}
+
+document.getElementById('stringInput').addEventListener('input', (event) => {
+  document.getElementById('output').style.visibility = 'visible';
+
+  let userInput = event.target.value;
+  let result = fms(userInput);
+
+  result = result == 1 ? 'TRUE' : 'FALSE';
+
+  document.getElementById('result').innerHTML = result;
+  document.getElementById('result').style.textAlign = 'center';
+});
