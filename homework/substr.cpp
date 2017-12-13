376955c2a6e2d9edc314aa5bebfc842c74685214
Domenico Maisano <dev.dmaisano@gmail.com>
2017-12-13 12:57:11 -0500

    finished substr.cpp

--- homework/substr.cpp
+++ homework/substr.cpp
@@ -7,49 +7,56 @@ bool fmsSubstr(string);
 void pushChar(vector<char>&, char);
 
 int main() {
-  vector<char> vect;
-  string input;
+  vector<char> vect; // vector to hold the binary string (only 1s and 0s)
+  string input; // string for the user input
 
   cout << "Enter a string: ";
   cin >> input;
 
+  // converts the string to a character array to iterate over
   const char* charArray = input.c_str();
 
+  // iterates through each character from the user's input
   for(int i = 0; i < input.length(); ++i) {
     switch(charArray[i]) {
-      case '0':
+      case '0': // if the current character is '0', push it to the vector
         pushChar(vect, charArray[i]);
         break;
 
-      case '1':
+      case '1': // if the current character is '1', push it to the vector
         pushChar(vect, charArray[i]);
         break;
       
-      default:
+      // the default case resumes the iterations of the loop and is run when the current character is neither a 0 or 1
+      default: 
         continue;
     }
   }
 
+  // converts the vector of characters to a string
   string str(vect.begin(), vect.end());
 
-  cout << "\n" << fmsSubstr(str);
-
+  // the conditional executes if a valid substring is found
   if(fmsSubstr(str))
-    cout << input << " is a valid substring!";
+    cout << input << " contains a valid substring of '1101'!";
 
+
+  // the conditional executes if a valid substring is not found
   else
-    cout << input << input << " is not a valid substring!";
+    cout << input << input << " does not contain a valid substring of '1101'!";
 
   cout << "\n\n";
   
   return 0;
 }
 
+// pushes the character to the char vector by reference without needing to specify a return type
 void pushChar(vector<char>& vect, char c) {
   if(c == '0' || c == '1')
     vect.push_back(c);
 }
 
+// function to validate the contents of the string to determine if it contains the substring '1101'
 bool fmsSubstr(string str) {
   if(str.find("1101") != string::npos)
     return true;
