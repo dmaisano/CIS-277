ae03ac232bbb251c5de41ca33d59947f1ba4115f
Domenico Maisano <dev.dmaisano@gmail.com>
2018-01-24 15:08:51 -0500

    updated gitignore

--- CS-280/project0.cpp
+++ CS-280/project0.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
   size md = { "medium", 10 };
   size lg = { "large", 20 };
 
-  if(argc <= 1)
+  if(argc < 1)
     cout << "MISSING SIZE" << endl;
 
   for(int i = 1; i < argc; i++) {
