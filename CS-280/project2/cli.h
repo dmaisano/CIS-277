35b7836ea2a9bfdebddc7cbe0a064e16a15789bd
Domenico Maisano <dev.dmaisano@gmail.com>
2018-02-26 01:39:11 -0500

    wip part 2

---
+++ CS-280/project2/cli.h
@@ -0,0 +1,68 @@
+#ifndef CLI_H
+#define CLI_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <set>
+#include "./project-2.h"
+using namespace std;
+
+class CLI {
+public:
+  // exposes the main functionality of the program
+  CLI(int argc, vector<string> argv) {
+    // stores a set of flag args that will be used when parsing
+    set<string> cliFlags, flags = { "-v", "-mci", "-sum" };
+    bool foundFile;
+    string fileName;
+
+    // exit quietly if no args
+    if(argc == 1) {
+      exit(0);
+    }
+
+    for(auto arg : argv) {
+      // flag handler
+      if(arg[0] == '-') {
+        // executes if valid flag is found
+        if(inFlags(flags, arg))
+          cliFlags.insert(arg);
+
+        else {
+          cout << "INVALID FLAG " << arg << endl;
+          exit(0);
+        }
+      }
+
+      // file handler
+      else if(arg[0] != '-') {
+        if(foundFile == false) {
+          fileName = arg;
+
+          // if file cannot be found
+          if(ifstream(fileName).fail()) {
+            cout << "UNABLE TO OPEN " << fileName << endl;
+            exit(0); 
+          }
+
+          foundFile = true;
+        }
+
+        // if a file is already found
+        else if(foundFile == true) {
+          cout << "TOO MANY FILE NAMES" << endl; exit(0);
+        }
+      }
+    }
+
+    // FINALLY. we lex the file!
+    Lexer();
+    
+
+    exit(0); // exit the program
+  }
+};
+
+#endif
--- CS-280/project2/lex.h
+++ CS-280/project2/lex.h
@@ -51,17 +51,17 @@ enum TType {
 
 class Token {
 	TType	  tt;     // token type
-	string  lexeme; // string to be lexed
+	string  lexeme; // unit of the language, one or more words
 	int			lnum;   // line number
 
 public:
-  // constructor if no parameters passed??
+  // constructor if no parameters passed
 	Token() {
 		tt = ERR;
 		lnum = -1;
 	}
 
-  // idk anymore lol
+  // constructor if parameters passed
 	Token(TType tt, string lexeme, int line) {
 		this->tt = tt;
 		this->lexeme = lexeme;
@@ -72,14 +72,23 @@ public:
 	bool operator == (const TType tt) const { return this->tt == tt; }
 	bool operator != (const TType tt) const { return this->tt != tt; }
 
-	TType		  GetTokenType()  const   { return tt; }
-	string		GetLexeme()     const   { return lexeme; }
-	int			  GetLinenum()    const   { return lnum; }
+  // ----member functions----
+  // returns the enum of the "token"
+	TType GetTokenType() const {
+    return tt;
+  }
+  // returns the "lexeme"
+	string GetLexeme() const {
+    return lexeme;
+  }
+  // returns the total number of lines
+	int	GetLinenum() const {
+    return lnum;
+  }
 };
 
-// functions defined but not yet written
+// function && operator defined, but not yet written
 extern ostream& operator << (ostream& out, const Token& tok);
-
 extern Token getNextToken(istream *in, int *linenum);
 
 
