2c35a73bf580f88c3a632ed636a4e874c9346561
Domenico Maisano <dev.dmaisano@gmail.com>
2018-03-05 14:53:41 -0500

    using provided project1.cpp solution

---
+++ CS-280/playground/basics/reduce.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// returns 0 if no list provided
+auto reduce = [](vector<int> nums = {0}) {
+  int sum = 0;
+
+  for(auto num : nums)
+    sum += num;
+
+  return sum;
+};
+
+int main() {
+  vector<int> nums = {1, 2, 3, 4, 5};
+
+  cout << reduce(nums);
+}
