a81f56c1e393e06ae205f1e2177903cf1d07b094
Domenico Maisano <dev.dmaisano@gmail.com>
2018-04-09 12:28:52 -0400

    added project 3 solution

---
+++ CS-280/project-4/cli.h
@@ -0,0 +1,61 @@
+  /*
+  * Domenico Maisano
+  * CS280
+  * Spring 2018
+  * 
+  * cli.h
+  */
+
+  #ifndef CLI_H
+  #define CLI_H
+
+  #include <iostream>
+  #include <cctype>
+  #include <fstream>
+  #include "./parser.h"
+  using namespace std;
+
+  namespace CLI {
+    // exposes the main functionality of the program
+    static void Parser(int argc, vector<string> argv) {
+      istream *in = &cin;
+      ifstream file;
+      bool traceMode = false;
+
+      for(auto arg : argv) {
+        // flag handler
+        if(arg[0] == '-') {
+          // executes if valid flag is found
+          if(arg == "-t")
+            traceMode = true;
+
+          else {
+            cout << "UNKNOWN FLAG " << arg << endl;
+            exit(0);
+          }
+        }
+        // error handler
+        else if(in != &cin) {
+          cerr << "TOO MANY FILENAMES" << endl;
+          exit(0);
+        }
+        // file handler
+        else {
+          file.open(arg);
+          if(file.is_open() == false) {
+            cerr << "COULD NOT OPEN " << arg << endl;
+            exit(0);
+          }
+        }
+
+        in = &file;
+      }
+      // finally we can actually run the program
+      Parser::Parse(in, traceMode);
+      // Interpreter::Interpret(in);
+      cout << endl;
+      exit(0);
+    }
+  };
+
+  #endif
---
+++ CS-280/project-4/project-4.cpp
@@ -0,0 +1,21 @@
+/*
+* Domenico Maisano
+* CS280
+* Spring 2018
+* 
+* project-3.cpp
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include "./cli.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  // argv vector
+  vector<string> args(argv + 1, argv + argc);
+
+  CLI::Parser(argc, args);
+}
