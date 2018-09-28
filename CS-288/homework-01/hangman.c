10c1ef52615aa9677d8367629ad8028ec66e6b2f
Domenico Maisano <dev.dmaisano@gmail.com>
2018-09-28 09:56:51 -0400

    changed to ansi

--- CS-288/homework-01/hangman.c
+++ CS-288/homework-01/hangman.c
@@ -1,5 +1,5 @@
-// Domenico Maisano
-// CS-288-101
+/* Domenico Maisano */
+/* CS-288-101 */
 
 #include <ctype.h>
 #include <stdio.h>
@@ -11,26 +11,26 @@ int i;
 /* define boolean type */
 typedef enum { false, true } bool;
 
-// global word variable
+/* global word variable */
 const char ACTUAL_WORD[] = "JAVASCRIPT";
 
-// function prototypes
+/* function prototypes */
 void game(char *);
 
 int main() {
-  // will not contain more than 80 chars
+  /* will not contain more than 80 chars */
   char word[81] = {};
 
-  // fill the array with placeholders
+  /* fill the array with placeholders */
   for (i = 0; i < strlen(ACTUAL_WORD); i++) {
     word[i] = '*';
   }
 
-  // welcome message
+  /* welcome message */
   printf("Welcome to Hangman!\n");
   printf("Try to guess the secret word one letter at a time.\n");
 
-  // play the game
+  /* play the game */
   game(word);
 }
 
@@ -40,16 +40,16 @@ void game(char *word) {
 
   bool matches = false;
 
-  // loop until an exit condition is met
+  /* loop until an exit condition is met */
   while (true) {
 
-    // print win message and exit
+    /* print win message and exit */
     if (!strcmp(word, ACTUAL_WORD)) {
       printf("\nCongratulations, You solved the word: '%s'\n", word);
       break;
     }
 
-    // print message if player lost and exit
+    /* print message if player lost and exit */
     if (guesses <= 0) {
       printf("\nYou ran out of guesses and lost.\n");
       break;
@@ -61,19 +61,19 @@ void game(char *word) {
     letter = toupper(letter);
 
     for (i = 0; i < strlen(ACTUAL_WORD); i++) {
-      // reset foundWord to false
+      /* reset foundWord to false */
       if (i == 0) {
         matches = false;
       }
 
       if (ACTUAL_WORD[i] == letter) {
-        // found match
+        /* found match */
         matches = true;
         word[i] = letter;
       }
 
-      // if a match was not found for the char...
-      // decrement # of guesses user has left
+      /* if a match was not found for the char... */
+      /* decrement # of guesses user has left */
       else if (matches == false && i == strlen(ACTUAL_WORD) - 1) {
         --guesses;
       }
