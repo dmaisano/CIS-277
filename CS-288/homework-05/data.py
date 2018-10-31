812d2998a3980ce93c9ebf3ceffa0aa82b306b8f
Domenico Maisano <dev.dmaisano@gmail.com>
2018-10-31 13:29:16 -0400

    ditching C using Python

--- CS-288/homework-05/data.py
+++ CS-288/homework-05/data.py
@@ -2,4 +2,24 @@
 
 # python script to generate data for the sorting algorithms
 
+import sys
+import random
 
+if len(sys.argv) > 1:
+    numItems = int(sys.argv[1])
+else:
+    print("no args")
+    exit(1)
+
+f = open("data.txt", "w+")
+
+for i in range(numItems):
+    num = random.randint(0, 1000000)
+
+    # f.write("%d\n" % num)
+
+    if (i == numItems - 1):
+        f.write("%d" % num)
+
+    else:
+        f.write("%d," % num)
