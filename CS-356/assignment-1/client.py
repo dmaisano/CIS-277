ef94b5eaa0229a8cd5c3dc711ec7db3d7df32fb7
Domenico Maisano <dev.dmaisano@gmail.com>
2019-01-26 20:59:00 -0500

    finished assignment 1

--- CS-356/assignment-1/client.py
+++ CS-356/assignment-1/client.py
@@ -1,26 +1,31 @@
 import sys
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM, timeout
 
-if len(sys.argv) < 3:
-    print("missing args")
-    exit(1)
+host = sys.argv[1] if len(sys.argv) >= 2 else "127.0.0.1"
+port = int(sys.argv[2]) if len(sys.argv) >= 3 else 3000
+count = int(sys.argv[3]) if len(sys.argv) >= 4 else 10
 
-host = sys.argv[1]
-port = int(sys.argv[2])
-count = int(sys.argv[3])
+# default bufferSize 4096 bytes
+bufferSize = 4096
 
 # data to be sent
 payload = "X" * count
 
 client = socket(AF_INET, SOCK_DGRAM)
 
-print("sending payload to %s:%s\npayload: %s" % (host, port, payload))
+numRetries = 0
+while numRetries < 3:
+    try:
+        client.sendto(payload.encode(), (host, port))
+        print("Sending data to  %s, %d: %s" % (host, port, payload))
 
-# send the data
-client.sendto(payload.encode(), (host, port))
-
-# setting large default buffer size
-bufferSize = 4096
-
-# receive a response
-response = client.recv(bufferSize)
+        client.settimeout(1)
+        data, serverAddress = client.recvfrom(bufferSize)
+    except timeout:
+        print("Message timed out")
+        numRetries += 1
+    except KeyboardInterrupt:
+        exit(0)
+    else:
+        print("Receive data from %s, %d: %s" % (*serverAddress, data.decode()))
+        break
--- CS-356/assignment-1/server.py
+++ CS-356/assignment-1/server.py
@@ -1,20 +1,31 @@
 import sys
-from socket import socket, AF_INET, SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM, timeout
 
-if len(sys.argv) < 2:
-    print("missing IP and port")
-    exit(1)
+ip = sys.argv[1] if len(sys.argv) >= 2 else "127.0.0.1"
+port = int(sys.argv[2]) if len(sys.argv) >= 3 else 3000
 
-ip = sys.argv[1]
-port = int(sys.argv[2])
-max_data_len = 4096
+# default bufferSize 4096 bytes
+bufferSize = 4096
 
 server = socket(AF_INET, SOCK_DGRAM)
 server.bind((ip, port))
 
-print("server is running: http://%s:%s" % (ip, str(port)))
+print("server is running %s:%d\n" % (ip, port))
 
-# listen for requests
+# listen for data packets
 while True:
-    request = server.recv(max_data_len)
-    print(request)
+    try:
+        server.settimeout(1)
+        data, clientAddress = server.recvfrom(bufferSize)
+    except timeout:
+        continue
+    # gracefully shut down the server
+    except KeyboardInterrupt:
+        server.close()
+        exit(0)
+    else:
+        print("Receive data from %s, %d: %s" % (*clientAddress, data.decode()))
+
+        server.sendto(data, clientAddress)
+        print("Sending data to  %s, %d: %s\n" % (*clientAddress, data.decode()))
+
