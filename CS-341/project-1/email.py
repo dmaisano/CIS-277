3d8c82c76c32ed6c8d89eb7cb014fea30edbfda0
Domenico Maisano <dev.dmaisano@gmail.com>
2019-02-18 23:59:40 -0500

    finished project

--- CS-341/project-1/email.py
+++ CS-341/project-1/email.py
@@ -16,29 +16,89 @@ state = {
     7: "q7",
     8: "q8",
     9: "q9",  # goal state
+    "trap": "trap",  # trap state
 }
 
 # determines the current state of a character, given the previous state
-def determineState(char, previousState, index, length):
+def determineState(char, previousState):
     char = str(char)
     currentState = ""
 
     if previousState == state[1] and char.islower():
-        previousState = state[1]
         currentState = state[2]
 
     elif previousState == state[2] and char.islower():
-        previousState = state[2]
         currentState = state[2]
 
-    # returns the current state, and new previous state
-    return [currentState, previousState]
+    elif previousState == state[2] and char == ".":
+        currentState = state[3]
+
+    elif previousState == state[3] and char.islower():
+        currentState = state[2]
+
+    elif previousState == state[2] and char == "@":
+        currentState = state[4]
+
+    elif previousState == state[4] and char.islower():
+        currentState = state[5]
+
+    elif previousState == state[5] and char.islower():
+        currentState = state[5]
+
+    elif previousState == state[5] and char == ".":
+        currentState = state[6]
+
+    elif previousState == state[6] and char != "c" and char.lower():
+        currentState = state[5]
+
+    elif previousState == state[6] and char == "c":
+        currentState = state[7]
+
+    # .c.
+    elif previousState == state[7] and char == ".":
+        currentState = state[6]
+
+    elif previousState == state[7] and char != "o" and char.lower():
+        currentState = state[5]
+
+    elif previousState == state[7] and char == "o":
+        currentState = state[8]
+
+    # .co.
+    elif previousState == state[8] and char == ".":
+        currentState = state[6]
+
+    elif previousState == state[8] and char != "m" and char.islower():
+        currentState = state[5]
+
+    # reached goal state
+    elif previousState == state[8] and char == "m":
+        currentState = state[9]
+
+    # .com.
+    elif previousState == state[9] and char == ".":
+        currentState = state[6]
+
+    # anything else results in being stuck in the self looping trap case
+    else:
+        currentState = state["trap"]
+
+    # returns the current state
+    return currentState
 
 
-# print("Project 1 for CS 341")
-# print("Semester: Spring 2019")
-# print("Written by: Domenico Maisano, dm583")
-# print("Instructor: Marvin Nakayama, marvin@njit.edu\n")
+# create the output file
+fileName = sys.argv[1] if len(sys.argv) >= 2 else "output.txt"
+outputFile = open(fileName, "w")
+
+
+print("Project 1 for CS 341")
+outputFile.write("Project 1 for CS 341" + "\n")
+print("Semester: Spring 2019")
+outputFile.write("Semester: Spring 2019" + "\n")
+print("Written by: Domenico Maisano, dm583")
+print("Instructor: Marvin Nakayama, marvin@njit.edu")
+outputFile.write("Instructor: Marvin Nakayama, marvin@njit.edu" + "\n")
 
 
 # no stdin
@@ -51,23 +111,30 @@ stdin = str(sys.stdin.read()).splitlines()
 # starting position of list
 index = 0
 
-
 while index < len(stdin):
     previousState = "q1"
 
     email = str(stdin[index + 1]) if stdin[index] == "y" else exit(0)
 
-    print(len(email))
-
-    print("initial state: %s" % (state[1]))
+    print("\nevaluating email: " + email)
+    outputFile.write("\nevaluating email: " + email + "\n")
+    print("initial state: q1")
+    outputFile.write("initial state: q1" + "\n")
 
     for charIndex in range(len(email)):
         char = str(email[charIndex])
 
-        currentState, previousState = determineState(
-            char, previousState, charIndex, len(email)
-        )
-        print("currentState: " + currentState)
+        currentState = determineState(char, previousState)
+        previousState = currentState
+        print(char + ", state: " + currentState)
+        outputFile.write(char + ", state: " + currentState + "\n")
+
+        if charIndex == len(email) - 1 and currentState == "q9":
+            print(email + " is a valid email")
+            outputFile.write(email + " is a valid email" + "\n")
+        elif charIndex == len(email) - 1 and currentState != "q9":
+            print(email + " is not a valid email")
+            outputFile.write(email + " is not a valid email" + "\n")
 
     # increment index
     index += 2
