b0469b7f8ecc74fb63cb46da17bb6fa50c190e88
Domenico Maisano <dev.dmaisano@gmail.com>
2019-02-18 23:24:24 -0500

    Update email.py

--- CS-341/project-1/email.py
+++ CS-341/project-1/email.py
@@ -5,6 +5,36 @@ import sys
 import os
 
 
+# possible states
+state = {
+    1: "q1",  # initial state
+    2: "q2",
+    3: "q3",
+    4: "q4",
+    5: "q5",
+    6: "q6",
+    7: "q7",
+    8: "q8",
+    9: "q9",  # goal state
+}
+
+# determines the current state of a character, given the previous state
+def determineState(char, previousState, index, length):
+    char = str(char)
+    currentState = ""
+
+    if previousState == state[1] and char.islower():
+        previousState = state[1]
+        currentState = state[2]
+
+    elif previousState == state[2] and char.islower():
+        previousState = state[2]
+        currentState = state[2]
+
+    # returns the current state, and new previous state
+    return [currentState, previousState]
+
+
 # print("Project 1 for CS 341")
 # print("Semester: Spring 2019")
 # print("Written by: Domenico Maisano, dm583")
@@ -21,25 +51,24 @@ stdin = str(sys.stdin.read()).splitlines()
 # starting position of list
 index = 0
 
-states = {1: "q1", 2: "q2", 3: "q3"}
-currentState = "start"
-previousState = ""
 
 while index < len(stdin):
+    previousState = "q1"
+
     email = str(stdin[index + 1]) if stdin[index] == "y" else exit(0)
 
-    # print(range(len(email)))
+    print(len(email))
+
+    print("initial state: %s" % (state[1]))
 
     for charIndex in range(len(email)):
         char = str(email[charIndex])
 
-        # check if state == q3
-        if char == "." and (charIndex + 3 <= len(email)):
-            currentState = states[3]
-            print(currentState)
-
-        # if char.islower():
-        # currentState = ""
+        currentState, previousState = determineState(
+            char, previousState, charIndex, len(email)
+        )
+        print("currentState: " + currentState)
 
     # increment index
     index += 2
+
