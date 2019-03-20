a5a82ab0f26b6362383b934fe1981203d266501c
Domenico Maisano <dev.dmaisano@gmail.com>
2019-03-20 12:18:26 -0400

    wip

--- CS-435/project-1/justify.py
+++ CS-435/project-1/justify.py
@@ -1,60 +1,117 @@
-from typing import List
+from typing import Any, List
 import sys
 import os
 
 
-def solveCost(arr: List[int], width: int) -> List[List[int], List[int]]:
+def justifyText(arr: List[int], width: int) -> List[Any]:
     n = len(arr)
 
-    # initialize a list of size n
-    dp = [0] * n
+    costs = [0] * n
 
-    # used to store the index of the last word in each line
-    ans = [0] * n
+    # Array in which finalWords[i] store index
+    # of last word in line starting with
+    # word arr[i].
+    finalWords = [0] * n
 
-    # if only one word is present, then the cost of the last line is zero
-    dp[n - 1] = 0
-    ans[n - 1] = n - 1
+    # If only one word is present then
+    # only one line is required. Cost
+    # of last line is zero. Hence cost
+    # of this line is zero. Ending point
+    # is also n-1 as single word is
+    # present.
+    costs[n - 1] = 0
+    finalWords[n - 1] = n - 1
 
-    # ???
+    # Make each word first word of line
+    # by iterating over each index in arr.
     for i in range(n - 2, -1, -1):
-        currentLen = -1
-        dp[i] = sys.maxsize
+        lineLength = -1
+        costs[i] = 2147483647
 
-        # add words to the current line
+        # Keep on adding words in current
+        # line by iterating from starting
+        # word upto last word in arr.
         for j in range(i, n):
-            currentLen += arr[j] + 1
 
-            # can't fit more words on currentLine
-            if currentLen > width:
+            # Update number of characters
+            # in current line. arr[j] is
+            # number of characters in
+            # current word and 1
+            # represents space character
+            # between two words.
+            lineLength += arr[j] + 1
+
+            # If limit of characters
+            # is violated then no more
+            # words can be added to
+            # current line.
+            if lineLength > width:
                 break
+
+                # If current word that is
+                # added to line is last
+                # word of arr then current
+                # line is last line. Cost of
+                # last line is 0. Else cost
+                # is square of extra spaces
+                # plus cost of putting line
+                # breaks in rest of words
+                # from j+1 to n-1.
             if j == n - 1:
                 cost = 0
             else:
-                cost = (width - currentLen) ** 3
+                cost = (width - lineLength) * (width - lineLength) + costs[j + 1]
 
-            # check if this arrangement gives the min cost
-            if cost < dp[i]:
-                dp[i] = cost
-                ans[i] = j
+                # Check if this arrangement gives
+                # minimum cost for line starting
+                # with word arr[i].
+            if cost < costs[i]:
+                costs[i] = cost
+                finalWords[i] = j
 
+                # Print starting index and ending index
+                # of words present in each line.
     i = 0
-    boundaries = []
+    bounds = []
     while i < n:
-        boundaries.append([i, ans[i]])
+        if i != 0:
+            bounds.append([i + 1, finalWords[i] + 1])
+        elif i == 0:
+            bounds.append([i, finalWords[i] + 1])
+        i = finalWords[i] + 1
 
-        # set the new index to the next word
-        i = ans[i] + 1
-
-    return boundaries
+    return bounds
 
 
-def printNeat(words: List[str], boundaries: List[List[int], List[int]]):
-    for i, j in boundaries:
+def printNeat(words: List[str], bounds: List[any], width: int):
+    text = ""
+
+    totalCost = 0
+
+    line = ""
+    for bound in bounds:
+        i = bound[0]
+        j = bound[1]
+        # print(i, j, range(i, j))
         for index in range(i, j):
-            print(words[index], end=" ")
+            word = words[index]
 
-        print()
+            if index != j - 1:
+                line += word + " "
+            else:
+                if len(line) == 60:
+                    line += word + "\n"
+                else:
+                    cost = width - len(line) + len(word)
+                    print("cost", cost)
+                    totalCost += cost
+                    line += word + "\n"
+
+            text += line
+            line = ""
+
+    # print(cost)
+    print(text)
 
 
 if __name__ == "__main__":
@@ -75,5 +132,8 @@ if __name__ == "__main__":
     for word in words:
         wordLengths.append(len(word))
 
-    solveCost(wordLengths, maxWidth)
+    # solveWordWrap(wordLengths, maxWidth)
+    bounds = justifyText(wordLengths, maxWidth)
+    # print(bounds)
+    printNeat(words, bounds, maxWidth)
 
