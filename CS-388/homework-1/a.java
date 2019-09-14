429427a89f0210386f5769289ffa25f237f942d1
Domenico Maisano <dev.dmaisano@gmail.com>
2019-09-13 23:54:57 -0400

    added 3a example

---
+++ CS-388/homework-1/a.java
@@ -0,0 +1,13 @@
+// 3a
+class A {
+	public static void main(String args[]) {
+		System.out.println("number\tsquare\tcube");
+
+		for (int i = 0; i <= 10; i++) {
+			int square = i * i;
+			int cube = i * i * i;
+			String output = String.format("%d\t%d\t%d", i, square, cube);
+			System.out.println(output);
+		}
+	}
+}
