a168db3a5b119324109daa02b0753abb4621c1ce
Domenico Maisano <dev.dmaisano@gmail.com>
2019-09-14 23:40:48 -0400

    added shapes cpp

---
+++ playground/misc/shapes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+  virtual float GetPermiter() = 0;
+  virtual float GetArea() = 0;
+};
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}
