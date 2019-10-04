675426651caad74e629b8fc2eaffb2f1bdeac879
Domenico Maisano <dev.dmaisano@gmail.com>
2019-10-03 22:12:31 -0400

    working on reading input file

--- CS-370/assignment-1/PCS.java
+++ CS-370/assignment-1/PCS.java
@@ -1,98 +1,53 @@
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+import java.util.List;
 
 class Domino {
-  private String top;
-  private String bottom;
-  private String key; // D1, D2, D3, or D4
-
-  public Domino(String top, String bottom) {
-    this.top = top;
-    this.bottom = bottom;
-
-    if (top == "c" && bottom == "cca") {
-      this.key = "D1";
-    } else if (top == "ac" && bottom == "ba") {
-      this.key = "D2";
-    } else if (top == "bb" && bottom == "b") {
-      this.key = "D3";
-    } else if (top == "ac" && bottom == "cb") {
-      this.key = "D4";
-    } else if (top == "" && bottom == "") {
-      this.key = "(EMPTY)";
-    } else {
-      this.key = "???";
-    }
-  }
-
-  public String Top() {
-    return this.top;
-  }
-
-  public String Bottom() {
-    return this.bottom;
-  }
-
-  public String Key() {
-    return this.key;
-  }
-
-  public String toString() {
-    return this.key;
-  }
+  public String top;
+  public String bottom;
 }
 
-class State {
-  private String topString;
-  private String bottomString;
-  private int depth;
-  ArrayList<Domino> dominoes = new ArrayList<Domino>(); // current dominoes in state (e.g. { D3, D2 })
-  ArrayList<Domino> children = new ArrayList<Domino>(); // max size of 4 (branching factor)
-
-  public State(Domino domino, int depth, ArrayList<Domino> dominoes) {
-    this.depth = depth;
-    this.dominoes = dominoes;
-    this.children = children;
-  }
-
-  // returns false if bottom is an invalid combination of the top
-  public Boolean isValid() {
-    for (Domino domino : this.dominoes) {
-      System.out.println(domino.Bottom());
-    }
-
-    return false;
-  }
-
-  // finds any possible children for the current node
-  public void bfs_branch() {
-
-  }
+class Graph {
+  private int maxQueueSize;
+  private int maxNumStates;
+  private int numDominoes;
+  private Boolean verboseMode;
 }
 
-/**
- * Post Correspondence Solver
- */
 public class PCS {
+
   public static void main(String[] args) {
-    Domino D1 = new Domino("c", "cca");
-    Domino D2 = new Domino("ac", "ba");
-    Domino D3 = new Domino("bb", "b");
-    Domino D4 = new Domino("ac", "cb");
+    if (args.length == 0) {
+      System.out.println("missing input file name");
+      System.exit(0);
+    }
 
-    Domino[] dominoPool = { D1, D2, D3, D4 };
+    String fileName = args[0];
 
-    State empty = new State(new Domino("", ""), 0, new ArrayList<Domino>(), new ArrayList<Domino>() {
-      {
-        add(D1);
-        add(D3);
+    readInputFile(fileName);
+  }
+
+  public static void readInputFile(String fileName) {
+
+    try {
+      List<String> lines = Files.readAllLines(new File(fileName).toPath(), Charset.defaultCharset());
+
+      for (String line : lines) {
+        System.out.println(line);
       }
-    });
 
-    bfs_search(empty);
+      System.out.printf("lines: %d", lines.size());
+    } catch (IOException e) {
+      System.out.printf("Unable to open file, \"%s\"\n", fileName);
+    } catch (Exception e) {
+      System.out.println("Error parsing input file");
+    }
   }
 
-  public static void bfs_search(State initialState) {
-
-  }
 }
