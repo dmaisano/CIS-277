14883d059b38656cc1149048932f963f312b1ed3
Domenico Maisano <dev.dmaisano@gmail.com>
2019-10-08 14:55:12 -0400

    wip

--- CS-370/assignment-1/PCS.java
+++ CS-370/assignment-1/PCS.java
@@ -4,16 +4,55 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 class Domino {
   public String top;
   public String bottom;
+
+  public Domino(String top, String bottom) {
+    this.top = top;
+    this.bottom = bottom;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s : %s", this.top, this.bottom);
+  }
+
+  @Override
+  public int hashCode() {
+    // overriding hashcode returned by Domino objects
+    String dominoString = this.top + this.bottom;
+
+    return dominoString.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+
+    if (obj == null)
+      return false;
+
+    if (getClass() != obj.getClass())
+      return false;
+
+    Domino other = (Domino) obj;
+    if (this.top == other.top && this.bottom == other.bottom)
+      return false;
+
+    return true;
+  }
 }
 
 class State {
-  String diff;
+  String diff; // + -
   ArrayList<Domino> dominoes;
   State parent;
 }
@@ -36,26 +75,45 @@ public class PCS {
 
     String fileName = args[0];
 
-    readInputFile(fileName);
-  }
+    int maxQueueSize = 0;
+    int maxNumStates = 0;
+    Boolean verboseMode = false;
+    int numDominoes = 0;
+    Map<String, Domino> dominoPool = new HashMap<String, Domino>();
 
-  public static void readInputFile(String fileName) {
-    private int maxQueueSize;
-    private int maxNumStates;
-    private int numDominoes;
-    private Boolean verboseMode;
-
+    // read the input file
     try {
       List<String> lines = Files.readAllLines(new File(fileName).toPath(), Charset.defaultCharset());
 
-      for (String line : lines) {
-        System.out.println(line);
+      maxQueueSize = Integer.parseInt(lines.get(0).trim());
+      maxNumStates = Integer.parseInt(lines.get(1).trim());
+      verboseMode = Boolean.parseBoolean(lines.get(2).trim());
+      numDominoes = Integer.parseInt(lines.get(3).trim());
+
+      for (int i = 1; i <= numDominoes; i++) {
+        String[] line = lines.get(i + 3).trim().split("\\s+");
+
+        String top = line[line.length - 2];
+        String bottom = line[line.length - 1];
+
+        Domino d = new Domino(top, bottom);
+        dominoPool.put("D" + i, d);
       }
     } catch (IOException e) {
       System.out.printf("Unable to open file, \"%s\"\n", fileName);
+      System.exit(1);
     } catch (Exception e) {
       System.out.println("Error parsing input file");
+      System.exit(1);
+    }
+
+    System.out.printf("max queue size: %d\n", maxQueueSize);
+    System.out.printf("max num states: %d\n", maxNumStates);
+    System.out.printf("verbose: %b\n", verboseMode);
+    System.out.printf("num dominoes: %d\n", numDominoes);
+
+    for (String key : dominoPool.keySet()) {
+      System.out.printf("key: %s, domino: %s\n", key, dominoPool.get(key));
     }
   }
-
 }
