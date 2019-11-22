ef6f4b5540c449678851a7eeda3df3c4476267fc
Domenico Maisano <dev.dmaisano@gmail.com>
2019-11-21 22:03:38 -0500

    added positional args

--- CS-370/assignment-3/classifier.py
+++ CS-370/assignment-3/classifier.py
@@ -5,6 +5,7 @@ Gradient-Based Classifier
 """
 
 from typing import List, Dict, Tuple
+from operator import itemgetter
 from sys import argv
 import argparse
 from os import path
@@ -12,20 +13,24 @@ from math import inf, isinf
 
 
 parser = argparse.ArgumentParser(description="Gradient-Based Classifier Program")
-parser.add_argument("-v", "--verbose", help="verbose output flag", default=False)
+
+parser.add_argument(
+    "file", type=str, default="", help="relative input file path",
+)
+parser.add_argument("step", action="store", type=float, help="step size")
+parser.add_argument("epsilon", action="store", type=float, help="epsilon")
+parser.add_argument("M", action="store", type=int, help="number of random restarts")
+parser.add_argument(
+    "-v", "--verbose", action="store_true", default=False, help="verbose output flag"
+)
 
 args = parser.parse_args()
 
-if len(argv) < 2:
-    print("Missing args")
-    exit(1)
+file, step, epsilon, M, verbose = itemgetter("file", "step", "epsilon", "M", "verbose")(
+    vars(args)
+)
 
-verboseMode = True if "-v" or "--verbose" in argv else False
-
-# assuming path is relative
-filePath: str = argv[1]
-
-inputFile: List[str] = open(filePath, "r").readlines()
+inputFile: List[str] = open(file, "r").readlines()
 
 trainingSet: Dict[str, List[int]] = {}
 
@@ -59,8 +64,4 @@ def gradDescent(
     print("gradDescent")
 
 
-stepSize = 0
-epsilon = 0
-M = 0
-
-gradDescent(trainingSet, stepSize, epsilon, M)
+gradDescent(trainingSet, step, epsilon, M)
